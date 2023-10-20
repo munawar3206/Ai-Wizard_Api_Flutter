6b3770013196890f3a9550c0c07814415e7d02f6
munawar3206 <munawarkuttimon68@gmail.com>
2023-10-20 14:06:43 +0530

    final

--- lib/constants/api_const.dart
+++ lib/constants/api_const.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
